148d6e09e6c66776e7c552398f4e061d07d36d91
Luis Lopez <lualopezpe@gmail.com>
2023-10-07 14:34:57 -0500

    minor adjustments to controllers

--- controllers/employees_controller.py
+++ controllers/employees_controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request
 from io import StringIO
-from .base_controller import insert_hist_data_from_csv
+from .base_controller import insert_hist_data_from_csv, insert_data_from_csv
 from models.employee import Employee
 
 employees_blueprint = Blueprint('employees', __name__)
@@ -9,4 +9,10 @@ employees_blueprint = Blueprint('employees', __name__)
 @employees_blueprint.route('/upload', methods=['POST'])
 def upload_employees():
     csv_data = StringIO(request.data.decode('utf-8'))
-    return insert_hist_data_from_csv(csv_data, Employee)
+    return insert_data_from_csv(csv_data, Employee)
+
+
+@employees_blueprint.route('/hist', methods=['GET'])
+def upload_hist():
+    file_path = request.args.get('file_path')
+    return insert_hist_data_from_csv(file_path, Employee)
--- controllers/jobs_controller.py
+++ controllers/jobs_controller.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 from io import StringIO
-from .base_controller import insert_hist_data_from_csv
+from .base_controller import insert_hist_data_from_csv, insert_data_from_csv
 from models.job import Job
 
 
@@ -10,4 +10,11 @@ jobs_blueprint = Blueprint('jobs', __name__)
 @jobs_blueprint.route('/upload', methods=['POST'])
 def upload_jobs():
     csv_data = StringIO(request.data.decode('utf-8'))
-    return insert_hist_data_from_csv(csv_data, Job)
+    return insert_data_from_csv(csv_data, Job)
+
+
+@jobs_blueprint.route('/hist', methods=['GET'])
+def upload_hist():
+    file_path = request.args.get('file_path')
+    return insert_hist_data_from_csv(file_path, Job)
+
